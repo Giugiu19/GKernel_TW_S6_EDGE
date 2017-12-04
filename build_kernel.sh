d977b1594359c2ac490bd72664774c7df6fa5fe4
GDevGroup <gpirasking@outlook.it>
2017-12-04 14:30:50 +0100

    Build_kernel.sh : Add Notimestamp & Enable Graphite Flags

--- build_kernel.sh
+++ build_kernel.sh
@@ -2,6 +2,8 @@
 
 export ARCH=arm64
 export CROSS_COMPILE=/home/giugiu19/aarch64-linaro-linux-android/bin/aarch64-linaro-linux-android-
+export KCONFIG_NOTIMESTAMP=true
+export ENABLE_GRAPHITE=true
 
 make ARCH=arm64 zeroltexx_defconfig
 make ARCH=arm64 -j4
